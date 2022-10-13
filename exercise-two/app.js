524b818a24d4a1ca92c22cf1c664a2066c6fc089
= <christopher.dent@opscompass.com>
2022-10-13 17:42:09 -0400

    add methods object with simple function to demo app

--- exercise-two/app.js
+++ exercise-two/app.js
@@ -1,13 +1,21 @@
 const app = Vue.createApp({
     data: function() {
+// equally could say data(){keys: values}, which means that the value of the data key is a function
+
         return{
             courseGoal: "Finish these exercises so I can help my team as much as I can!",
             howSo: "Vue is interpolating this text into the HTML page, wow!",
             evenArray: ['Even Arrays!', 'Calvin', 'Chilito', 'Julia', 'Angie', 'Chris'],
             cjdLink: 'http://www.christopher-dent.com'
         }
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random()           
+                if (randomNumber < 0.5) {return "play with vue"} 
+                else {return "master vue"}            
+        }
     }
-    // equally could say data(){keys: values}, which means that the value of the data key is a function
 });
 
 
