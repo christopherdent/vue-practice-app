32e5041f20e39d54d72058a62c1d71454fa58e15
= <christopher.dent@opscompass.com>
2022-10-14 14:02:39 -0400

    exercise complete

--- exercise-two/app.js
+++ exercise-two/app.js
@@ -1,15 +1,12 @@
 const app = Vue.createApp({
-    data: function() {
+    data() {
 // equally could say data(){keys: values}, which means that the value of the data key is a function
-
-        return{
+        return {
             myName: "Chris",
-            myAge: "39",
-            oldMe: "50",
+            myAge: 39,
             favNum: Math.random(),
             cjdLink: "http://www.christopher-dent.com",
             houseStark: "https://static.wikia.nocookie.net/gameofthrones/images/8/8a/House-Stark-Main-Shield.PNG/revision/latest?cb=20170101103142"
-
         }
     },
     methods: {
@@ -17,7 +14,11 @@ const app = Vue.createApp({
             min = Math.ceil(0)
             max = Math.floor(1)
             return Math.floor(Math.random() * (max - min + 1) + min);
+        },
+        calculateAge() {
+            return this.myAge + 5;
         }
+
     }
 });
 
